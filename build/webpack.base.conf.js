548fbb4a4943a4927840de2e9d91662c08fade44
HeavenZhangHr <240964503@qq.com>
2017-08-03 18:44:15 +0800

    去除了重复模块打包

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,11 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
+var webpack = require('webpack')
+let HappyPack = require('happypack');
+let os = require('os');
+let happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -26,31 +31,65 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      // options: vueLoaderConfig
+      options: {
+        loaders: {
+          js: 'happypack/loader?id=js' // 将loader换成happypack
         }
       }
+    },
+    {
+      test: /\.js$/,
+      // loader: 'babel-loader',
+      // loader: ['babel-loader?cacheDirectory=true'],
+      loader: ['happypack/loader?id=js'], // 将loader换成happypack
+      include: [resolve('src'), resolve('test')],
+      exclude: [path.resolve('../../node_modules')] // 不需要编译node_modules下的js 
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('img/[name].[hash:7].[ext]')
+      }
+    },
+    {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+      }
+    }
     ]
-  }
+  },
+  cache: true,
+  plugins: [
+    new HappyPack({
+      id: 'vue',
+      threadPool: happyThreadPool,
+      cache: true,
+      verbose: true,
+      loaders: ['vue-loader'],
+    }),
+
+    new HappyPack({
+      id: 'js',
+      threadPool: happyThreadPool,
+      cache: true,
+      verbose: true,
+      loaders: ['babel-loader'],
+    }),
+
+    // 提取公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'shared-module',
+      minChunks: (module, count) => (
+        count >= 2    // 当一个模块被重复引用2次或以上的时候单独打包起来。 
+      )
+    }),
+  ]
 }
